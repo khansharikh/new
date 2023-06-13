ef83b09eeb2a08a852a02626281608fb0529409e
khansharikh <sknkkhan47@gmail.com>
2023-06-13 18:00:48 -0400

    first commit

---
+++ src/main/java/level2/docker/assignment/AssignmentApplication.java
@@ -0,0 +1,13 @@
+package level2.docker.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/level2/docker/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package level2.docker.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
